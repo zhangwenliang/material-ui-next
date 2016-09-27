ec8537b9e1424741c9a5cd02d3a734e23d639fd9
Wenliang <wenliang.zhang@hpe.com>
2016-09-27 10:24:56 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var APP_DIR = path.join(__dirname, 'src');
+var BUILD_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+	entry: APP_DIR + '/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        exclude: 'node_modules',
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
